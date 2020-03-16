5205d41deb94a40ae7605eaa6a4c6c99eb714524
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-03-16 10:35:27 -0600

    In class commit @ 1035

--- src/main/java/edu/cnm/deepdive/qod/service/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/qod/service/QuoteRepository.java
@@ -1,10 +1,10 @@
 package edu.cnm.deepdive.qod.service;
 
-import edu.cnm.deepdive.qod.model.entity.Quote;
-import java.util.Optional;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+    import edu.cnm.deepdive.qod.model.entity.Quote;
+    import java.util.Optional;
+    import java.util.UUID;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Query;
 
 public interface QuoteRepository extends JpaRepository<Quote, UUID> {
 
